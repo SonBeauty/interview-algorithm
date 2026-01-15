4a566e4517c9e89c81e4b461eb65819042506b32
son <sont9779@gmail.com>
2026-01-15 23:51:02 +0700

    island

---
+++ numOffices.js
@@ -0,0 +1,47 @@
+// input grid:
+//         [[1, 1, 0, 0, 0],
+//          [1, 1, 0, 0, 0],
+//          [0, 0, 1, 0, 0],
+//          [0, 0, 0, 1, 1]]
+// Output
+// 3
+
+function numOffices(grid = []) {
+  if (grid.length === 0) return;
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let count = 0;
+
+  //create dfs
+  function dfs(r, c) {
+    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 0) {
+      return;
+    }
+
+    grid[r][c] = 0;
+    dfs(r + 1, c);
+    dfs(r - 1, c);
+    dfs(r, c + 1);
+    dfs(r, c - 1);
+  }
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 1) {
+        count++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log(
+  numOffices([
+    [1, 1, 0, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1],
+  ])
+);
